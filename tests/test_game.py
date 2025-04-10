16d5e3e0cf63bb4878c2d58685fc60e88f866943
Macksym-Savchenko <macksymsnow@gmail.com>
2025-04-10 21:15:59 +0300

    add function test_play_level_next_level

--- tests/test_game.py
+++ tests/test_game.py
@@ -48,3 +48,34 @@ class TestGame:
         game.play_levels()
 
         assert game.level_number == 1
+
+    @patch("src.game.car.Car")
+    @patch("src.game.l.Level")
+    @patch("src.game.gi.GameInfo")
+    @patch("src.game.cl.Collision.handle_collision")
+    @patch("src.game.pygame.display.set_mode", return_value=MagicMock())
+    @patch("src.game.pygame.font.SysFont", return_value=MagicMock(render=MagicMock()))
+    def test_play_level_next_level(self, mock_font, mock_display, mock_collision, mock_game_info_class,
+                                   mock_level_class, mock_car_class, game):
+        game.running = True
+        game.handle_events = MagicMock()
+        game.draw = MagicMock()
+
+        mock_game_info = MagicMock()
+        mock_game_info.started = True
+        mock_game_info.get_time.return_value = 10
+        mock_game_info_class.return_value = mock_game_info
+
+        mock_collision.return_value = ("next_level", 10)
+
+        def fake_tick(val):
+            game.running = False
+
+        mock_clock = MagicMock()
+        mock_clock.tick.side_effect = fake_tick
+
+        with patch("src.game.pygame.time.Clock", return_value=mock_clock):
+            result = game.play_level()
+
+        assert result == "next_level"
+        mock_collision.assert_called()
