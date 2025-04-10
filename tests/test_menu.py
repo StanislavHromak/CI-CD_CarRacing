2f47deb96019e67f298c108ed1a27a7823daa8f0
Macksym-Savchenko <macksymsnow@gmail.com>
2025-04-10 20:30:12 +0300

    create function test_draw

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -53,3 +53,24 @@ class TestMenu:
         mock_keys.return_value = FakeKeyPress()
         result = mock_menu.handle_input()
         assert result is None
+
+    @patch("src.menu.pygame.display.update")
+    @patch("src.menu.pygame.font.Font")
+    def test_draw(self, mock_font_class, mock_display_update, mock_menu):
+        fake_surface = MagicMock()
+        fake_surface.get_rect.return_value = MagicMock(center=(0, 0))
+
+        mock_font = MagicMock()
+        mock_font.render.return_value = fake_surface
+        mock_font_class.return_value = mock_font
+
+        # вручну призначаємо замокані шрифти, бо вони створені у конструкторі
+        mock_menu.font = mock_font
+        mock_menu.title_font = mock_font
+
+        mock_menu.draw()
+
+        assert mock_menu.win.fill.called
+        assert mock_font.render.call_count == len(mock_menu.buttons) + 1
+        assert mock_menu.win.blit.call_count == len(mock_menu.buttons) + 1
+        assert mock_display_update.called
